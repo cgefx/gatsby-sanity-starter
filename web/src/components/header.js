2323bfa509cc34dab04ef93557fbfe75642dc1b7
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-10-19 16:20:57 -0700

    remove old dark mode toggler for now

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,24 +1,12 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import ThemeToggler from './themeToggler'
 
-const Header = ({ siteTitle, theme, toggleTheme }) => (
+const Header = ({ siteTitle }) => (
   <header>
     <div>
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-          <div>
-            <ThemeToggler theme={theme} toggleTheme={toggleTheme} />
-          </div>
-        </Link>
+        <Link to="/">{siteTitle}</Link>
       </h1>
     </div>
   </header>
--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
-import { GlobalStyles, lightTheme, darkTheme } from '../styles'
-import { useDarkMode } from '../utils/useDarkMode'
 import Header from './header'
 import 'normalize.css'
 
@@ -18,32 +15,11 @@ const Layout = ({ children }) => {
     }
   `)
 
-  const [theme, toggleTheme, componentMounted] = useDarkMode()
-  const themeMode = theme === 'light' ? lightTheme : darkTheme
-
-  // The default theme is loaded first and then checks local storage.
-  // If a different one is found some flicker can happen.
-  // This check blocks the app from loading until theme checking is done.
-  // if (!componentMounted) {
-  //   return <div />
-  // }
-
-  if (!componentMounted) {
-    return <div />
-  }
-
   return (
-    <ThemeProvider theme={themeMode}>
-      <>
-        <GlobalStyles />
-        <Header
-          theme={theme}
-          toggleTheme={toggleTheme}
-          siteTitle={data.site.siteMetadata?.title || `Title`}
-        />
-        <main>{children}</main>
-      </>
-    </ThemeProvider>
+    <>
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <main>{children}</main>
+    </>
   )
 }
 
