2dad38605d3aba12ad59ee704b683938cb7cfe1c
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-10-28 18:19:51 -0700

    remove type npm

--- web/src/styles/typography.js
+++
@@ -1,6 +0,0 @@
-import Typography from 'typography'
-import elkGlen from 'typography-theme-elk-glen'
-
-const typography = new Typography(elkGlen)
-export const { scale, rhythm, options } = typography
-export default typography
