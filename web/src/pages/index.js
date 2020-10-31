f2748d1a60706e2af21279a77dc6ec4ce06d420d
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-10-30 17:09:54 -0700

    add contact to home page

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -3,12 +3,14 @@ import { Link } from 'gatsby'
 import SEO from '../components/SEO'
 import Hero from '../components/Hero'
 import About from '../components/About'
+import Contact from '../components/Contact'
 
 const IndexPage = () => (
   <>
     <SEO title="Home" />
     <Hero />
     <About />
+    <Contact />
     {/* <Link to="/projects/">Go to Projects</Link> <br /> */}
   </>
 )
