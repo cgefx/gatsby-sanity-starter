73d339c9f2cf2d74f9040fdf36e96555a1babcf6
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-11-09 18:05:58 -0800

    query featured projext from home

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,20 +1,56 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import SEO from '../components/SEO'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import Contact from '../components/Contact'
 import FeaturedProjects from '../components/FeaturedProjects'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <>
     <SEO title="Home" />
     <Hero />
     <About />
-    <FeaturedProjects />
+    <FeaturedProjects projects={data.featuredProjects.nodes} />
     <Contact />
-    {/* <Link to="/projects/">Go to Projects</Link> <br /> */}
   </>
 )
+
+export const query = graphql`
+  query {
+    featuredProjects: allSanityProject(
+      limit: 3
+      filter: { featured: { eq: true } }
+    ) {
+      nodes {
+        title
+        description
+        featured
+        image {
+          asset {
+            fluid(maxWidth: 800) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+        repoLink
+        liveLink
+        slug {
+          current
+        }
+        techTags {
+          name
+          image {
+            asset {
+              fixed(width: 32) {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
