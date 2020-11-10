88b13d88c1b1129fd1c69c540d6beb636085e8f9
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-11-09 18:02:56 -0800

    add shared section

---
+++ web/src/components/_shared/styled-section.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const StyledSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 95%;
+  padding: 30px 0;
+  max-width: 1000px;
+  margin-bottom: 60px;
+`
+
+export const StyledFullHeightSection = styled(StyledSection)`
+  min-height: calc(100vh - var(--header-height));
+  justify-content: start;
+`
