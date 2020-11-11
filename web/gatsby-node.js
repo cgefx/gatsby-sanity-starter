25fde991554b57baf0128a18cea9b144e4fd73df
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-11-10 17:07:56 -0800

    create single project pages

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -1,7 +1,32 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  // 1. Get a template for this page
+
+  const projectTemplate = path.resolve('./src/templates/Project.js')
+
+  // 2. Query all pizzas
+  const { data } = await graphql(`
+    query {
+      projects: allSanityProject {
+        nodes {
+          title
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `)
+
+  // 3. Loop over each pizza and create a page for it
+  data.projects.nodes.forEach(project => {
+    actions.createPage({
+      path: `project/${project.slug.current}`,
+      component: projectTemplate,
+      context: {
+        slug: project.slug.current,
+      },
+    })
+  })
+}
