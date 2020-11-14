47ea61a9e69df5a60996c6eafeec443904840180
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-11-13 20:06:11 -0800

    remove typeography js

--- web/src/styles/Typography.js
+++ web/src/styles/Typography.js
@@ -2,10 +2,9 @@ import { createGlobalStyle } from 'styled-components'
 
 const Typography = createGlobalStyle`
 	body {
-		
-      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-     font-weight: 400;
-  font-size: 1rem;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
 		line-height: 1.75;
 	}
 
@@ -13,142 +12,134 @@ const Typography = createGlobalStyle`
 		${'' /* margin-bottom: 1.25em; */}
 	}
 
-	${'' /* TAnia from here on */}
-	h1,
-h2,
-h3,
-h4,
-h5 {
-  margin: 0 0 1.5rem 0;
-  font-weight: 700;
-  line-height: 1.2;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+	${'' /* Tania from here on */}
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    margin: 0 0 1.5rem 0;
+    font-weight: 700;
+    line-height: 1.2;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-h1:not(:first-child),
-h2:not(:first-child),
-h3:not(:first-child),
-h4:not(:first-child) {
-  margin-top: 3rem;
-}
+  h1:not(:first-child),
+  h2:not(:first-child),
+  h3:not(:first-child),
+  h4:not(:first-child) {
+    margin-top: 3rem;
+  }
 
-h1 {
-  font-size: 2.5rem;
-  line-height: 1.1;
-}
-
-h2 {
-  font-size: 1.75rem;
-  padding-bottom: 0.5rem;
- 
-}
-
-h2 code {
-  font-size: 1.75rem !important;
-}
-
-h3 {
-  font-size: 1.5rem;
-  ${'' /* color: var(--font-color); */}
-  font-weight: 600;
-  margin-bottom: 1rem;
-}
-
-h3 code {
-  font-size: 1.4rem !important;
-}
-
-h4 {
-  font-size: 1.3rem;
-  ${'' /* color: var(--font-color); */}
-  font-weight: 500;
-  margin-bottom: 1rem;
-  ${'' /* border-bottom: 2px solid var(--light-background); */}
-  padding-bottom: 0.25rem;
-}
-
-h5 {
-  font-size: 1.2rem;
-  margin-bottom: 1rem;
-}
-
-@media screen and (min-width: 800px) {
   h1 {
-    font-size: 3rem;
+    font-size: 2.5rem;
   }
 
   h2 {
-    font-size: 1.9rem;
+    font-size: 1.75rem;
+    padding-bottom: 0.5rem;
   }
 
   h2 code {
-    font-size: 1.9rem !important;
+    font-size: 1.75rem !important;
   }
 
   h3 {
-    font-size: 1.7rem;
-    ${'' /* color: var(--font-color); */}
+    font-size: 1.5rem;
     font-weight: 600;
+    margin-bottom: 1rem;
   }
 
   h3 code {
-    font-size: 1.6rem !important;
+    font-size: 1.4rem !important;
   }
 
   h4 {
-    font-weight: 400;
-    font-size: 1.4rem;
+    font-size: 1.3rem;
+    font-weight: 500;
+    margin-bottom: 1rem;
+    padding-bottom: 0.25rem;
   }
-}
 
+  h5 {
+    font-size: 1.2rem;
+    margin-bottom: 1rem;
+  }
 
+  @media screen and (min-width: 800px) {
+    h1 {
+      font-size: 3rem;
+    }
 
-	p,
-ol,
-ul,
-dl,
-table,
-blockquote {
-  font-size: 1.05rem;
-  margin: 0 0 1.5rem 0;
-}
+    h2 {
+      font-size: 1.9rem;
+    }
 
-ul {
-  padding: 0 1rem;
-}
+    h2 code {
+      font-size: 1.9rem !important;
+    }
 
-@media screen and (min-width: 800px) {
+    h3 {
+      font-size: 1.7rem;
+      font-weight: 600;
+    }
+
+    h3 code {
+      font-size: 1.6rem !important;
+    }
+
+    h4 {
+      font-weight: 400;
+      font-size: 1.4rem;
+    }
+  }
+
   p,
   ol,
   ul,
   dl,
   table,
   blockquote {
-    font-size: 1.125rem;
+    font-size: 1.05rem;
+    margin: 0 0 1.5rem 0;
   }
 
   ul {
-    padding: 0 2rem;
+    padding: 0 1rem;
   }
-}
 
-ul li p {
-  margin: 0;
-}
+  @media screen and (min-width: 800px) {
+    p,
+    ol,
+    ul,
+    dl,
+    table,
+    blockquote {
+      font-size: 1.125rem;
+    }
 
-ul li ul {
-  padding-left: 1rem;
-  margin: 0;
-}
+    ul {
+      padding: 0 2rem;
+    }
+  }
 
-ul li ul li {
-  margin: 0;
-}
+  ul li p {
+    margin: 0;
+  }
 
-ol li ol {
-  margin-bottom: 0;
-}
+  ul li ul {
+    padding-left: 1rem;
+    margin: 0;
+  }
+
+  ul li ul li {
+    margin: 0;
+  }
+
+  ol li ol {
+    margin-bottom: 0;
+  }
 
 ${'' /* until here */}
 
